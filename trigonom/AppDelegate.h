6cd21ef176ce7e86099ada4aeca0eebf4b637025
Anton Orzes <anton.orzes@gmail.com>
2016-06-05 06:52:30 +0200

    Initial Commit

---
+++ trigonom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  trigonom
+//
+//  Created by Anton Orzes on 05/06/16.
+//  Copyright © 2016 Anton Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ trigonom/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  trigonom
+//
+//  Created by Anton Orzes on 05/06/16.
+//  Copyright © 2016 Anton Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
